526c480ebb42890fb5e3d06e9baa0f083921e3bc
klu2300033050 <pasalaharsha1@gmail.com>
2026-02-20 18:14:39 +0530

    refactor: remove complex password restrictions from backend

--- src/controllers/auth-controllers/citizen.auth.controller.ts
+++ src/controllers/auth-controllers/citizen.auth.controller.ts
@@ -9,13 +9,6 @@ const signupSchema = z.object({
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters" })
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-      {
-        message:
-          "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-      }
-    )
     .trim(),
   email: z.string().email({ message: "Invalid email format" }).trim(),
   phonenumber: z
